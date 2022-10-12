6e7e4c0899263945ac5bec4928455f6e11c7df86
jatinshanbhag <115632456+jatinshanbhag@users.noreply.github.com>
2022-10-12 18:13:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,10 @@ streamlit.text("Sausages")
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+
+
+import pandas
+my_fruit_list = pandas.("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dateframe(my_fruit_list)
