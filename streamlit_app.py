9acc5990be25affc5ce9ec026ab980543118d4b6
jatinshanbhag <115632456+jatinshanbhag@users.noreply.github.com>
2022-10-12 18:05:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 streamlit.title("My Parents New Healthy Dinner")
 
+
 streamlit.header("Breakfast Menu")
+streamlit.text("🥣 🥗 🐔 🥑🍞")
 streamlit.text("Bread and Egg")
 streamlit.text("Sausages")
